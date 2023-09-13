e008505937ba101a31ff812dbf6d1c461d2a0bae
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-09-13 14:30:05 +0545

    Removed unnecessary files and routing added with basic pages and component setup

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Home = () => (
+  <div>
+    <Header title="This is header" back="false" />
+  </div>
+);
+
+export default Home;
